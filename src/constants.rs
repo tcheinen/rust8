69306df9899a2585b561b0ae9ad5a89c6a6108b9
Teddy Heinen <skyshayde@gmail.com>
2019-09-01 16:03:52 -0500

    add license

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,19 @@
+// Rust8
+// Copyright (C) 2019 Teddy Heinen
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 pub const FONTSET: [u8; 5 * 16] = [
     0xF0, 0x90, 0x90, 0x90, 0xF0,        // 0
     0x20, 0x60, 0x20, 0x20, 0x70,        // 1
