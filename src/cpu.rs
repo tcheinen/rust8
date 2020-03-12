896c83be1c6be73badd711aefea8fc651aeb5a15
Teddy Heinen <teddy@heinen.dev>
2020-03-12 18:45:53 -0500

    rename CPUOperationAppliable trait to CPU put CPU trait in module add State.tick()

--- src/cpu.rs
+++ src/cpu.rs
@@ -14,228 +14,230 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use crate::State;
+mod cpu {
+    use crate::state::{*};
 
-trait CPUOperationAppliable {
-    fn clear(&mut self);
-    fn ret(&mut self);
-    fn jump(&mut self);
-    fn call(&mut self);
-    fn skip_equal_byte(&mut self);
-    fn skip_neq_byte(&mut self);
-    fn skip_equal(&mut self);
-    fn load_byte(&mut self);
-    fn add_byte(&mut self);
-    fn load(&mut self);
-    fn or(&mut self);
-    fn and(&mut self);
-    fn xor(&mut self);
-    fn add(&mut self);
-    fn sub(&mut self);
-    fn shr(&mut self);
-    fn subn(&mut self);
-    fn shl(&mut self);
-    fn skip_neq(&mut self);
-    fn load_index_byte(&mut self);
-    fn jump_r0_byte(&mut self);
-    fn rand(&mut self);
-    fn draw(&mut self);
-    fn skip_key(&mut self);
-    fn skip_neq_key(&mut self);
-    fn load_reg_delay(&mut self);
-    fn load_reg_key(&mut self);
-    fn load_delay_reg(&mut self);
-    fn load_sound_reg(&mut self);
-    fn add_i_reg(&mut self);
-    fn load_sprite(&mut self);
-    fn load_bcd(&mut self);
-    fn load_mem_registers(&mut self);
-    fn load_registers_mem(&mut self);
-}
-
-impl CPUOperationAppliable for State {
-    // 0x00E0 - Clear the display
-    fn clear(&mut self) {
-        vram = [false; 64 * 32]
+    pub trait CPU {
+        fn clear(&mut self);
+        fn ret(&mut self);
+        fn jump(&mut self);
+        fn call(&mut self);
+        fn skip_equal_byte(&mut self);
+        fn skip_neq_byte(&mut self);
+        fn skip_equal(&mut self);
+        fn load_byte(&mut self);
+        fn add_byte(&mut self);
+        fn load(&mut self);
+        fn or(&mut self);
+        fn and(&mut self);
+        fn xor(&mut self);
+        fn add(&mut self);
+        fn sub(&mut self);
+        fn shr(&mut self);
+        fn subn(&mut self);
+        fn shl(&mut self);
+        fn skip_neq(&mut self);
+        fn load_index_byte(&mut self);
+        fn jump_r0_byte(&mut self);
+        fn rand(&mut self);
+        fn draw(&mut self);
+        fn skip_key(&mut self);
+        fn skip_neq_key(&mut self);
+        fn load_reg_delay(&mut self);
+        fn load_reg_key(&mut self);
+        fn load_delay_reg(&mut self);
+        fn load_sound_reg(&mut self);
+        fn add_i_reg(&mut self);
+        fn load_sprite(&mut self);
+        fn load_bcd(&mut self);
+        fn load_mem_registers(&mut self);
+        fn load_registers_mem(&mut self);
     }
 
-    // 0x00EE - Return from a function
-    fn ret(&mut self) {
-        sp -= 1;
-        pc = stack[sp];
-    }
-
-    // 0x1nnn - Jump to location nnn
-    fn jump(&mut self) {
-        pc = get_u16();
-    }
-
-    // 0x2nnn - Call function at nnn
-    fn call(&mut self) {
-        stack[sp] = pc;
-        sp += 1;
-        pc = get_u16();
-    }
-
-    // 0x3xkk - Skip next operation if register x is equal to kk
-    fn skip_equal_byte(&mut self) {
-        if registers[get_vx() == get_u8()] {
-            pc += 2;
+    impl CPU for State {
+        // 0x00E0 - Clear the display
+        fn clear(&mut self) {
+            self.vram = [false; 64 * 32]
         }
-    }
 
-    // 0x4xkk - Skip next operation if register x is not equal to kk
-    fn skip_neq_byte(&mut self) {
-        if registers[get_vx() != get_u8()] {
-            pc += 2;
+        // 0x00EE - Return from a function
+        fn ret(&mut self) {
+            self.sp -= 1;
+            self.pc = self.stack[self.sp];
         }
-    }
 
-    // 0x5xy0 - Skip next operation if register x is equal to register y
-    fn skip_equal(&mut self) {
-        if get_vx() == get_vy() {
-            pc += 2;
+        // 0x1nnn - Jump to location nnn
+        fn jump(&mut self) {
+            self.pc = self.get_u16();
         }
-    }
 
-    // 0x6xkk - Load byte kk into register x
-    fn load_byte(&mut self) {
-        registers[get_vx()] = get_u8()
-    }
-
-
-    // 0x7xkk - Add byte kk to register x
-    fn add_byte(&mut self) {
-        registers(get_vx()) += get_u8();
-    }
-
-    // 0x8xy0 - Load register y into register x
-    fn load(&mut self) {
-        registers[get_vx()] = registers[get_vy()]
-    }
-
-
-    // 0x8xy1 - Set register x to bitwise or with register y
-    fn or(&mut self) {
-        registers[get_vx()] |= registers[get_vy()]
-    }
-
-    // 0x8xy2 - Set register x to bitwise and with register y
-    fn and(&mut self) {
-        registers[get_vx()] &= registers[get_vy()]
-    }
-
-    // 0x8xy3 - Set register x to bitwise xor with register y
-    fn xor(&mut self) {
-        registers[get_vx()] ^= registers[get_vy()]
-    }
-
-    // 0x8xy4 - Add register y to register x, set register F to 1 if carry
-    fn add(&mut self) {
-        let sum: u16 = registers[get_vx()] + registers[get_vy()];
-        registers[0xf] = if sum > 255 { 1 } else { 0 };
-        registers[get_vx()] = sum & 0xff
-    }
-
-    // 0x8xy5 - Subtract register y from register x, set register F to 1 if register x > register y
-    fn sub(&mut self) {
-        registers[0xf] = if get_vx() > get_vy() { 1 } else { 0 };
-        registers[get_vx()] -= registers[get_vy()]
-    }
-
-    // 0x8xy6 - Set register f to lsb and shift register x right 1
-    fn shr(&mut self) {
-        registers[0xf] = registers[get_vx()] & 0x1;
-        registers[get_vx()] >>= 1;
-    }
-
-    // 0x8xy7 - subtract Vx from Vy and store in Vx, if Vy > Vx then Vf = 1
-    fn subn(&mut self) {
-        registers[0xf] = if registers[get_vy()] > registers[get_vx()] { 1 } else { 0 };
-        registers[get_vx()] = registers[get_vy()] - registers[get_vx()]
-    }
-
-    // 0x8xy8 - Set register f to msb and shift register x left 1
-    fn shl(&mut self) {
-        registers[0xf] = (registers[get_vx()] & 0x80) >> 7;
-        registers[get_vx()] <<= 1;
-    }
-
-
-    // 0x9xy0 - Skip next operation if Vx != Vy
-    fn skip_neq(&mut self) {
-        if registers[get_vx()] != registers[get_vy()] {
-            pc += 2;
+        // 0x2nnn - Call function at nnn
+        fn call(&mut self) {
+            self.stack[self.sp] = self.pc;
+            self.sp += 1;
+            self.pc = self.get_u16();
         }
-    }
 
-    // 0xAnnn - Index is set to nnn
-    fn load_index_byte(&mut self) {
-        index = get_u16();
-    }
-
-    // 0xBnnn - PC is set to V0 + nnn
-    fn jump_r0_byte(&mut self) {
-        pc = registers[0x0] + get_u16();
-    }
-
-
-    // 0xCxkk - Generate random byte, AND with kk, and then store in Vx
-    fn rand(&mut self) {
-        let mut rng = rand::thread_rng();
-        registers[self.get_vx()] = rng.gen::<u8>() & self.get_u8()
-    }
-
-    fn draw(&mut self) {}
-
-    // 0xEx9E - skip if key is pressed
-    fn skip_key(&mut self) {
-        if self.keypad[self.get_vx()] {
-            self.pc += 2
+        // 0x3xkk - Skip next operation if register x is equal to kk
+        fn skip_equal_byte(&mut self) {
+            if self.registers[self.get_vx() == self.get_u8()] {
+                self.pc += 2;
+            }
         }
-    }
 
-    // 0xExA1 - skip if key is not pressed
-    fn skip_neq_key(&mut self) {
-        if !self.keypad[self.get_vx()] {
-            self.pc += 2
+        // 0x4xkk - Skip next operation if register x is not equal to kk
+        fn skip_neq_byte(&mut self) {
+            if self.registers[self.get_vx() != self.get_u8()] {
+                self.pc += 2;
+            }
         }
-    }
 
-    // 0xFx07 - load delay into register vx
-    fn load_reg_delay(&mut self) {
-        self.registers[self.get_vx()] = self.delay
-    }
+        // 0x5xy0 - Skip next operation if register x is equal to register y
+        fn skip_equal(&mut self) {
+            if self.get_vx() == self.get_vy() {
+                self.pc += 2;
+            }
+        }
 
-    fn load_reg_key(&mut self) {
-        unimplemented!()
-    }
+        // 0x6xkk - Load byte kk into register x
+        fn load_byte(&mut self) {
+            self.registers[self.get_vx()] = self.get_u8()
+        }
 
-    fn load_delay_reg(&mut self) {
-        unimplemented!()
-    }
 
-    fn load_sound_reg(&mut self) {
-        unimplemented!()
-    }
+        // 0x7xkk - Add byte kk to register x
+        fn add_byte(&mut self) {
+            self.registers(self.get_vx()) += self.get_u8();
+        }
 
-    fn add_i_reg(&mut self) {
-        unimplemented!()
-    }
+        // 0x8xy0 - Load register y into register x
+        fn load(&mut self) {
+            self.registers[self.get_vx()] = registers[self.get_vy()]
+        }
 
-    fn load_sprite(&mut self) {
-        unimplemented!()
-    }
 
-    fn load_bcd(&mut self) {
-        unimplemented!()
-    }
+        // 0x8xy1 - Set register x to bitwise or with register y
+        fn or(&mut self) {
+            self.registers[self.get_vx()] |= self.registers[self.get_vy()]
+        }
 
-    fn load_mem_registers(&mut self) {
-        unimplemented!()
-    }
+        // 0x8xy2 - Set register x to bitwise and with register y
+        fn and(&mut self) {
+            self.registers[self.get_vx()] &= self.registers[self.get_vy()]
+        }
 
-    fn load_registers_mem(&mut self) {
-        unimplemented!()
+        // 0x8xy3 - Set register x to bitwise xor with register y
+        fn xor(&mut self) {
+            self.registers[self.get_vx()] ^= self.registers[self.get_vy()]
+        }
+
+        // 0x8xy4 - Add register y to register x, set register F to 1 if carry
+        fn add(&mut self) {
+            let sum: u16 = self.registers[self.get_vx()] + self.registers[self.get_vy()];
+            self.registers[0xf] = if sum > 255 { 1 } else { 0 };
+            self.registers[self.get_vx()] = sum & 0xff
+        }
+
+        // 0x8xy5 - Subtract register y from register x, set register F to 1 if register x > register y
+        fn sub(&mut self) {
+            self.registers[0xf] = if self.get_vx() > self.get_vy() { 1 } else { 0 };
+            self.registers[self.get_vx()] -= self.registers[self.get_vy()]
+        }
+
+        // 0x8xy6 - Set register f to lsb and shift register x right 1
+        fn shr(&mut self) {
+            self.registers[0xf] = self.registers[self.get_vx()] & 0x1;
+            self.registers[self.get_vx()] >>= 1;
+        }
+
+        // 0x8xy7 - subtract Vx from Vy and store in Vx, if Vy > Vx then Vf = 1
+        fn subn(&mut self) {
+            self.registers[0xf] = if self.registers[self.get_vy()] > self.registers[self.get_vx()] { 1 } else { 0 };
+            self.registers[self.get_vx()] = self.registers[self.get_vy()] - self.registers[self.get_vx()]
+        }
+
+        // 0x8xy8 - Set register f to msb and shift register x left 1
+        fn shl(&mut self) {
+            self.registers[0xf] = (self.registers[self.get_vx()] & 0x80) >> 7;
+            self.registers[self.get_vx()] <<= 1;
+        }
+
+
+        // 0x9xy0 - Skip next operation if Vx != Vy
+        fn skip_neq(&mut self) {
+            if self.registers[self.get_vx()] != self.registers[self.get_vy()] {
+                self.pc += 2;
+            }
+        }
+
+        // 0xAnnn - Index is set to nnn
+        fn load_index_byte(&mut self) {
+            self.index = self.get_u16();
+        }
+
+        // 0xBnnn - PC is set to V0 + nnn
+        fn jump_r0_byte(&mut self) {
+            self.pc = self.registers[0x0] as u16 + self.get_u16();
+        }
+
+
+        // 0xCxkk - Generate random byte, AND with kk, and then store in Vx
+        fn rand(&mut self) {
+            let mut rng = rand::thread_rng();
+            self.registers[self.get_vx()] = rng.gen::<u8>() & self.get_u8()
+        }
+
+        fn draw(&mut self) {}
+
+        // 0xEx9E - skip if key is pressed
+        fn skip_key(&mut self) {
+            if self.keypad[self.get_vx()] {
+                self.pc += 2
+            }
+        }
+
+        // 0xExA1 - skip if key is not pressed
+        fn skip_neq_key(&mut self) {
+            if !self.keypad[self.get_vx()] {
+                self.pc += 2
+            }
+        }
+
+        // 0xFx07 - load delay into register vx
+        fn load_reg_delay(&mut self) {
+            self.registers[self.get_vx()] = self.delay
+        }
+
+        fn load_reg_key(&mut self) {
+            unimplemented!()
+        }
+
+        fn load_delay_reg(&mut self) {
+            unimplemented!()
+        }
+
+        fn load_sound_reg(&mut self) {
+            unimplemented!()
+        }
+
+        fn add_i_reg(&mut self) {
+            unimplemented!()
+        }
+
+        fn load_sprite(&mut self) {
+            unimplemented!()
+        }
+
+        fn load_bcd(&mut self) {
+            unimplemented!()
+        }
+
+        fn load_mem_registers(&mut self) {
+            unimplemented!()
+        }
+
+        fn load_registers_mem(&mut self) {
+            unimplemented!()
+        }
     }
 }
--- src/state.rs
+++ src/state.rs
@@ -15,6 +15,7 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 use crate::constants;
+use crate::cpu::CPU;
 
 pub struct State {
     pub registers: [u8; 16],
@@ -31,11 +32,78 @@ pub struct State {
 }
 
 impl State {
+    pub fn tick(&mut self) {
+        let instruction: u16 = (self.memory[pc] << 8) | self.memory[pc + 1];
+        let lower_4: u8 = (instruction & 0xff) as u8;
+        let lower_8: u8 = (instruction & 0xff) as u8;
+        let lower_8: u8 = (instruction & 0xff) as u8;
+
+        let x: u8 = ((instruction >> 8) & 0xf0) as u8;
+        let n: u8 = (instruction & 0x000f) as u8;
+        let nn: u8 = (instruction & 0x00ff) as u8;
+
+        match instruction >> 12 {
+            0x0 => {
+                match nn {
+                    0xE0 => { self.clear() }
+                    0xEE => { self.ret() }
+                    _ => { panic!("Invalid Opcode {:#X} at PC {:#x}", instruction, self.pc) }
+                }
+            }
+            0x1 => { self.jump() }
+            0x2 => { self.call() }
+            0x3 => { self.skip_equal_byte() }
+            0x4 => { self.skip_neq_byte() }
+            0x5 => { self.skip_equal() }
+            0x6 => { self.load_byte() }
+            0x7 => { self.add_byte() }
+            0x8 => {
+                match n {
+                    0x0 => { self.load() }
+                    0x1 => { self.or() }
+                    0x2 => { self.and() }
+                    0x3 => { self.xor() }
+                    0x4 => { self.add() }
+                    0x5 => { self.sub() }
+                    0x6 => { self.shr() }
+                    0x7 => { self.subn() }
+                    0x8 => { self.shl() }
+                    _ => { panic!("Invalid Opcode {:#X} at PC {:#x}", instruction, self.pc) }
+                }
+            }
+            0x9 => { self.skip_neq() }
+            0xA => { self.load_index_byte() }
+            0xB => { self.jump_r0_byte() }
+            0xC => { self.rand() }
+            0xD => { self.draw() }
+            0xE => {
+                match nn {
+                    0x9e => { self.skip_key() }
+                    0xA1 => { self.skip_neq_key() }
+                    _ => { panic!("Invalid Opcode {:#X} at PC {:#x}", instruction, self.pc) }
+                }
+            }
+            0xF => {
+                match nn {
+                    0x07 => { self.load_reg_delay() }
+                    0x0A => { self.load_reg_key() }
+                    0x15 => { self.load_delay_reg() }
+                    0x18 => { self.load_sound_reg() }
+                    0x1E => { self.add_i_reg() }
+                    0x29 => { self.load_sprite() }
+                    0x33 => { self.load_bcd() }
+                    0x55 => { self.load_mem_registers() }
+                    0x65 => { self.load_registers_mem() }
+                    _ => { panic!("Invalid Opcode {:#X} at PC {:#x}", instruction, self.pc) }
+                }
+            }
+            _ => { panic!("Invalid Opcode {:#X} at PC {:#x}", instruction, self.pc) }
+        }
+    }
 
     // Read u16 from memory and increment PC twice
-    pub fn read_cycle(&self) -> u16 {
-        opcode: u16 = self.memory[pc] | self.memory[pc+1];
-        self.pc += 2;
+    pub fn read_cycle(&mut self) -> u16 {
+        opcode: u16 = self.memory[pc] | self.memory[pc + 1];
         opcode
     }
 
@@ -73,7 +141,7 @@ impl Default for State {
             sound: 0,
             keypad: [false; 16],
             vram: [false; 64 * 32],
-            opcode: 0
+            opcode: 0,
         };
         for i in 0..constants::FONTSET.len() {
             state.memory[constants::FONTSET_START + i as u16] = constants::FONTSET[i];
